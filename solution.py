8bdacfdf45ccb47dd5ef1a5e2e567481ba550211
gip200 <71387169+gip200@users.noreply.github.com>
2020-09-20 13:44:27 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -60,7 +60,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
 
    # Send message data.
    # Fill in start
-   message = raw_input("Enter your message data: \r\n")
+   message = "Enter your message: \r\n"
    # Fill in end
 
    # Message ends with a single period.
